3d6ae6c2371ded4d560c1a990a60d502ca41bb56
nicogarzonhub <nicogarzon654@gmail.com>
2025-11-18 19:52:45 -0500

    HU2.py

--- HU2.py
+++ HU2.py
@@ -1,9 +1,13 @@
+# Hacemos la lista del inventario de los productos
 inventario = []
 
+# Creamos una funcion para agregar el nombre,cantidad y precio del producto
 def agregar_producto():
     nombre = input("Ingresa el nombre del producto: ")
     cantidad = int(input("Ingresa la cantidad del producto: "))
     precio = float(input("Ingresa el precio del producto: "))
+
+#   Aqui conectamos las variables
 
     producto = {
         "nombre": nombre,
@@ -14,7 +18,7 @@ def agregar_producto():
     inventario.append(producto)
     print("Producto agregado!\n")
 
-
+# Luego otra funcion para mostrar el inventario si hay productos registrados
 def mostrar_inventario():
     if len(inventario) == 0:
         print("No hay productos registrados\n")
@@ -24,7 +28,7 @@ def mostrar_inventario():
             print(p)
         print()
 
-
+ # y la funcion para calcular las estadisticas de los productos es decir precio * cantidad
 def calcular_estadisticas():
     if len(inventario) == 0:
         print("No hay productos para calcular estadísticas\n")
@@ -33,11 +37,11 @@ def calcular_estadisticas():
     total_items = sum(p["cantidad"] for p in inventario)
     total_valor = sum(p["cantidad"] * p["precio"] for p in inventario)
 
-    print("\n=== ESTADÍSTICAS ===")
+    print("\n ESTADÍSTICAS ")
     print("Total de artículos:", total_items)
     print("Valor total del inventario:", total_valor, "\n")
 
-
+#Menu principal para el funcionamiento
 while True:
     print("= MENÚ PRINCIPAL =")
     print("1. Agregar producto")
